babb7bfdf621c6eef003cf9ce5eec938c8d46cd7
samsonpaul <samsonpaul4@gmail.com>
2016-11-11 10:53:13 +0300

    finding my way around the unittests

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -4,16 +4,16 @@ from logic import PrepaidBanking
 
 class PrepaidCard_TestCase(unittest.TestCase):
 	def test_prepaid_deposit(self):
-		self.assertEqual(PrepaidCard.deposit(50), (50 + amount))
+		self.assertEqual(PrepaidBanking.deposit(50), (50 + amount))
 
 	def test_prepaid_deposit(self):
-		self.assertEqual(PrepaidCard.deposit(0), msg="Your card has not been recharged")
+		self.assertEqual(PrepaidBanking.deposit(0), msg="Your card has not been recharged")
 
 	def tes_prepaid_deposit(self):
-		self.assertEqual(PrepaidCard.deposit(-4), msg="Invalid transaction attempt!")
+		self.assertEqual(PrepaidBanking.deposit(-4), msg="Invalid transaction attempt!")
 
 	def test_prepaid_online_payment(self):
-		self.assertEqual(PrepaidCard.online_payment(10), msg="You can't go beyond the card minimum balance")
+		self.assertEqual(PrepaidBanking.online_payment(10), msg="You can't go beyond the card minimum balance")
 
 	def test_prepaid_online_payment(self):
 		self.assertEqual(PrepaidBanking.online_payment(0), msg="No charges were incurred")
@@ -28,19 +28,19 @@ class PrepaidCard_TestCase(unittest.TestCase):
 		self.assertEqual(PrepaidBanking.online_payment(250), 300)
 
 	def test_premium_prepaid_deposit(self):
-		self.assertEqual(PrepaidBanking.PremiumPrepaidCard(50), 100)
+		self.assertEqual(PrepaidBanking.deposit(50), 100)
 
 	def test_premium_prepaid_deposit(self):
-		self.assertEqual(PrepaidBanking.PremiumPrepaidCard(45), msg="Insufficient deposit amount")
+		self.assertEqual(PrepaidBanking.deposit(45), msg="Insufficient deposit amount")
 
 	def test_premium_prepaid_deposit(self):
-		self.assertEqual(PrepaidBanking.PremiumPrepaidCard(50), msg="You can't go beyond the card minimum balance")
+		self.assertEqual(PrepaidBanking.online_payment(50), msg="You can't go beyond the card minimum balance")
 
 	def test_premium_prepaid_deposit(self):
-		self.assertEqual(PrepaidBanking.PremiumPrepaidCard(0), msg="No charges were incurred")
+		self.assertEqual(PrepaidBanking.online_payment(0), msg="No charges were incurred")
 
 	def test_premium_prepaid_deposit(self):
-		self.assertEqual(PrepaidBanking.PremiumPrepaidCard(900), 990)
+		self.assertEqual(PrepaidBanking.deposit(900), 990)
 
 if __name__ == "__main__":
 	unittest.main()
