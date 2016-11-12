74bad98d7ad61320dc3b229216a98ca0b4a59c09
samsonpaul <samsonpaul4@gmail.com>
2016-11-12 07:49:28 +0300

    more refining

--- app/test_logic.py
+++ app/test_logic.py
@@ -1,29 +1,24 @@
 import unittest
 
-from app import logic
+from logic import PrepaidCard, PremiumPrepaidCard
 
 class PrepaidCard_TestCase(unittest.TestCase):
-	def setUp(self):
-		self.logic = PrepaidCard()
-
-	def tearDown(self):
-		del self.logic
 
 	def test_prepaid_card_is_instance_of_prepaid_banking(self):
-		self.assertTrue(isinstance(self.logic, PrepaidBanking), msg='PrepaidCard is not a subclass of PrepaidBanking')
+		self.assertTrue(isinstance(self.PrepaidCard), msg='PrepaidCard is not a subclass of PrepaidBanking')
 
 	def test_prepaid_card_can_deposit_valid_amounts(self):
-		balance = self.logic.deposit(100)
+		balance = self.PrepaidCard.deposit(100)
 		self.assertEquals(balance, 100)
 
 	def test_prepaid_card_cannot_spend_more_than_current_balance(self):
-		message = self.logic.withdraw(100)
+		message = self.PrepaidCard.withdraw(100)
 		self.assertEquals(message, 'Cannot spend beond the current account balance', msg="You can't go beynd the card minimum balance")
 
 	def test_prepaid_card_can_spend_valid_cash_amounts(self):
-		self.logic.deposit(600)
-		self.logic.online_payment(500)
-		self.assertEquals(self.logic.balance, 100, msg='Incorrect balance after withdrawal')
+		self.PrepaidCard.deposit(600)
+		self.PrepaidCard.online_payment(500)
+		self.assertEquals(self.PrepaidCard.balance, 100, msg='Incorrect balance after withdrawal')
 
 
 class PremiumPrepaidCardTestCases(unittest.TestCase):
@@ -34,20 +29,20 @@ class PremiumPrepaidCardTestCases(unittest.TestCase):
 		del self.prem
 
 	def test_premium_prepaid_card_is_instance_of_prepaid_banking(self):
-		self.assertTrue(isinstance(self.logic, PrepaidBanking), msg='PrepaidCard is not a subclass of PrepaidBanking')
+		self.assertTrue(isinstance(self.PremiumPrepaidCard, PrepaidBanking), msg='PrepaidCard is not a subclass of PrepaidBanking')
 
 	def test_premium_prepaid_card_can_deposit_valid_amounts(self):
-		balance = self.logic.deposit(100)
+		balance = self.PremiumPrepaidCard.deposit(100)
 		self.assertEquals(balance, 100)
 
 	def test_premium_prepaid_card_cannot_spend_more_than_current_balance(self):
-		message = self.logic.withdraw(100)
+		message = self.PremiumPrepaidCard.withdraw(100)
 		self.assertEquals(message, 'Cannot spend beond the current account balance', msg="You can't go beynd the card minimum balance")
 
 	def test_prepaid_card_can_spend_valid_cash_amounts(self):
-		self.logic.deposit(600)
-		self.logic.online_payment(500)
-		self.assertEquals(self.logic.balance, 100, msg='Incorrect balance after withdrawal')
+		self.PremiumPrepaidCard.deposit(600)
+		self.PremiumPrepaidCard.online_payment(500)
+		self.assertEquals(self.PremiumPrepaidCard.balance, 100, msg='Incorrect balance after withdrawal')
 
 
 if __name__ == "__main__":
